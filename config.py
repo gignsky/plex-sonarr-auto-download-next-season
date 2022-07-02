67d5944efa51ccfa65b00ab3a1cfbbb68a125cb3
gignsky <gignsky@gmail.com>
2022-07-02 01:51:29 -0400

    redefined config

--- config.py
+++ config.py
@@ -1,29 +1,35 @@
-"""CONFIGURATION
+def initialConfigs():
+    """CONFIGURATION
 
-Returns:
-    initial config variables
-"""
+    Returns:
+        initial config variables
+    """
+    # General
+    LOG_LEVEL = 1
+    # Only Full Season at this time. Perhaps 'First Episode' in the future?
+    DOWNLOAD_TARGET = "FULL_SEASON"
 
-# START HERE
+    # START HERE
 
-# Determines how many episodes before the end of the season a new season should be downloaded.
-EPISODE_THRESHOLD = 2
+    # Determines how many episodes before the end of the season a new season should be downloaded.
+    EPISODE_THRESHOLD = 2
 
-# Plex
-PLEX_URL = "http://IP_TO_PLEX_SERVER:32400/"
-PLEX_TOKEN = "PLEX_TOKEN"
-PLEX_TV_SHOWS_LIBRARY = "TV_SHOW_LIBRARY_NAME_FROM_PLEX"
+    # Plex
+    PLEX_URL = "http://IP_TO_PLEX_SERVER:32400/"
+    PLEX_TOKEN = "PLEX_TOKEN"
+    PLEX_TV_SHOWS_LIBRARY = "TV_SHOW_LIBRARY_NAME_FROM_PLEX"
 
-# Sonarr
-SONARR_URL = "http://IP_TO_SONARR:8989"
-SONARR_API_KEY = "API_KEY"
+    # Sonarr
+    SONARR_URL = "http://IP_TO_SONARR:8989"
+    SONARR_API_KEY = "API_KEY"
 
-
-# STATIC
-# define printline
-PRINTLINE = "============================================="
-
-LOG_LEVEL = 1
-
-# Only Full Season at this time. Perhaps 'First Episode' in the future?
-DOWNLOAD_TARGET = "FULL_SEASON"
+    return (
+        LOG_LEVEL,
+        EPISODE_THRESHOLD,
+        DOWNLOAD_TARGET,
+        PLEX_URL,
+        PLEX_TOKEN,
+        PLEX_TV_SHOWS_LIBRARY,
+        SONARR_URL,
+        SONARR_API_KEY,
+    )
