bea9ab901f3eb513a550273c7757bf6134a2978e
gignsky <gignsky@gmail.com>
2023-01-21 01:32:03 -0500

    update outputs.py, plex_interactive.py and sonarr_interactive.py via chatgpt

--- logic/interactive_functions/outputs.py
+++ logic/interactive_functions/outputs.py
@@ -3,29 +3,35 @@
 
 
 def print_met_threshold():
-    print("     Met threshold for downloading new episodes.")
+    """Prints message indicating threshold met for downloading new episodes"""
+    print("Met threshold for downloading new episodes.")
 
 
-def print_can_NOT_find_sonarr_show():
+def print_cannot_find_sonarr_show():
+    """Prints error message when series not found in Sonarr"""
     print(
-        "Failed to fetch series from Sonarr. Please double check your Sonarr URL and API Key"
+        "Failed to fetch series from Sonarr. Please double check your Sonarr URL and API Key."
     )
 
 
 def print_instructing_sonarr():
-    print("     Instructing Sonarr to Monitor & Download the next season.")
+    """Prints message indicating instructions sent to Sonarr to monitor and download next season"""
+    print("Instructing Sonarr to Monitor & Download the next season.")
 
 
 def print_successful_request():
+    """Prints message indicating a successful request"""
     print("Request Sent -- SUCCESSFULLY")
 
 
 def print_failed_request(code):
-    print(
-        "     FAILED -- to process command. Received status code "
-        + str(code.status_code)
-    )
+    """Prints message indicating a failed request
+    Args:
+        code (int): status code of the failed request
+    """
+    print("FAILED -- to process command. Received status code " + str(code.status_code))
 
 
 def print_test_mode_on():
+    """Prints message indicating test mode is on"""
     print("Test-mode turned ON if it were turned off command be SUCCESSFUL")
--- logic/interactive_functions/plex_interactive.py
+++ logic/interactive_functions/plex_interactive.py
@@ -1,36 +1,34 @@
 from plexapi.server import PlexServer
 
 
-def grabPlexDetails(PLEX_URL, PLEX_TOKEN):
-    """grab plex info for server owner
+def grab_plex_details(PLEX_URL, PLEX_TOKEN):
+    """Grabs Plex info for server owner
 
     Args:
-        PLEX_URL (string): plex url
-        PLEX_TOKEN (string): plex token
+        PLEX_URL (str): Plex server URL
+        PLEX_TOKEN (str): Plex server token
 
     Returns:
-        plex_server: plex_server api integration
-        server_owner_account: plex server owner account
+        plex_server: Plex server API integration
+        server_owner_account: Plex server owner account
     """
-    # grab plex details
     plex_server = PlexServer(PLEX_URL, PLEX_TOKEN)
     server_owner_account = plex_server.myPlexAccount()
     return plex_server, server_owner_account
 
 
-def tryPlexUser(currentAccountOriginal, plex):
-    """Switch to another plex user
+def try_plex_user(current_account_original, plex):
+    """Tries to switch to another Plex user
 
     Args:
-        currentAccountOriginal: current account worker is processing
-        plex (plex_server - api): api access to server
+        current_account_original: Current account worker is processing
+        plex (plex_server - api): API access to server
 
     Returns:
-        _type_: _description_
+        str: "failed" if switching user fails, otherwise the new user's account
     """
     try:
-        x = plex.switchUser(currentAccountOriginal)
-        return x
+        new_user_account = plex.switchUser(current_account_original)
+        return new_user_account
     except:
-        x = "failed"
-        return x
+        return "failed"
